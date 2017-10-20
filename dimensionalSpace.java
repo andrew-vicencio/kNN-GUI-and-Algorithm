dcba3f97f621549f6d1c3c00d07f5db03a489f68
Andrew Clarence Vicencio <andrew.v1124@gmail.com>
2017-10-20 11:52:04 -0400

    Started outline of dimensionalSpace

---
+++ dimensionalSpace.java
@@ -0,0 +1,25 @@
+public class dimensionalSpace {
+  HashMap<String, Integer> mean;
+  HashMap<String, Integer> stddev;
+  HashMap<String, Integer> sum;
+  HashMap<String, Integer> n;
+  HashMap<String, Point> pts;
+
+  public class dimensionalSpace (){
+    
+  }
+
+  public class addPts(Point pt){
+
+  }
+
+  private class findMean(){
+  }
+
+  private class findStdDev(){
+  }
+
+  public class FindkNN(){
+  }
+
+}
