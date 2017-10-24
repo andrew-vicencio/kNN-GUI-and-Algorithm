5b3e33b61a235396178cf5fd2b7c6c04dce2ea0d
Andrew Clarence Vicencio <andrew.v1124@gmail.com>
2017-10-24 18:31:08 -0400

    Added comments to Points statistical methods. Sort of fixed findStdDev implementation.

--- Point.java
+++ Point.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.AbstractMap;
 import java.util.ArrayList;
+import java.lang.Math;
 
 public class Point 
 {
@@ -39,15 +40,32 @@ public class Point
 		return rawValues.get(att);
 	}
 	
+  /*
+  * standardise uses the value (X) and mean (u) of every attribute,
+  * as well as the number of points (n) to standardize according to a 
+  * a standard distribution using the formula:
+  * (X - stdDev)/u
+  */
   public void standardise(AbstractMap<String, Integer> mean, int n){
     for (String attr : attributes){
       Integer X = rawValues.get(attr);
       stdValues.put(attr, (X - findStdDev(X, mean.get(attr), n))/mean.get(attr));
     }
   }
-
+  
+  /*
+  * findStdDev finds the population stdDeviation by using the
+  * formula:
+  * (X - u)(p(x))
+  * where X is the value of the attribute, u is the mean
+  * of the attribute, and p(x) is the probability of the attribute
+  * in this case just 1 divided by the number of points (n)
+  *
+  * TODO: Change this, this is population stdDev but can do
+  * sample stdDev in dimensionalSpace
+  */
   private int findStdDev(Integer X, Integer mean, int n){
-    return ((X - mean)/n);
+    return (Math.pow((X - mean),2)/n);
   }
 
 	public void setCost(int cost)
