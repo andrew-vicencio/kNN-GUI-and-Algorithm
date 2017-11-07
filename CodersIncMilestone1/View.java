0f296aa898330cdfb0132b78a18b5a9a98126beb
Gaby Hubert <gabyhubert2@gmail.com>
2017-11-06 23:24:38 -0500

    createDataSet()
    Started method that allows users to initialize a set of data features once they choose the "New Data Set" menu option. To Do in comments

--- CodersIncMilestone1/View.java
+++ CodersIncMilestone1/View.java
@@ -1,12 +1,28 @@
+import java.awt.BorderLayout;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import javax.swing.*;
+/**
+ * View class for the GUI. This class is responsible for initiating the UI frame, and updating it as required
+ *
+ * @version Milestone 2
+ * @author Gabrielle
+ */
+
 
 public class View {
-	JFrame mainFrame;
-	JMenuBar menuBar;
-	JMenu create, edit, display, help;
-	JMenuItem newDataSet, newTestCase, addValue, helpDoc;
-	JPanel mainPanel;
+	private JFrame mainFrame;
+	private JMenuBar menuBar;
+	private JMenu create, edit, display, help;
+	private JMenuItem newDataSet, newTestCase, addValue, helpDoc;
+	private JPanel mainPanel;
+	private HashMap<String, String> dataType;
 	
+	 /**
+     * Initializes and displays the initial view
+     *
+     */
 	public View()
 	{
 		mainFrame = new JFrame("CODERS INC");
@@ -21,6 +37,7 @@ public class View {
 		addValue = new JMenuItem("Add Value");
 		helpDoc = new JMenuItem("View Help Documents");
 		
+		
 		mainFrame.setJMenuBar(menuBar);
 		menuBar.add(create);
 		menuBar.add(edit);
@@ -32,16 +49,57 @@ public class View {
 		help.add("View User Guide");
 		
 		mainFrame.setSize(500, 500);
+		mainPanel.setLayout(new BoxLayout(mainPanel, 0));
 		newTestCase.setEnabled(false);
 		edit.setEnabled(false);
 		display.setEnabled(false);
 		mainFrame.setVisible(true);
 		
-		
-		
-		
-		
+		dataType = new HashMap<String, String>();
 		
 	}
+	 /**
+     * Allows user to initialize a data set. 
+     * First, the user is prompted for the number of features required.
+     * Next, the user provides the name and class for each feature.
+     *
+     */
+	public HashMap<JTextField, JTextField> createDataSet()
+		{
+			JFrame numDataFrame = new JFrame();
+			// TO DO: error handling for non-int types OR restrict user from entering non-numerical data
+			int numData = Integer.parseInt((String)JOptionPane.showInputDialog(
+	                numDataFrame,
+	                "Enter the number of data features required: ",
+	                " ", JOptionPane.QUESTION_MESSAGE,
+	                null,
+	                null,
+	                null));
+			
+			//TO DO: Show this in mainPanel rather than in individual alert boxes
+			for(int i = 0; i < numData; i++)
+			{
+				JFrame featureFrame= new JFrame();
+				String key = (String)JOptionPane.showInputDialog(
+		                featureFrame,
+		                "Enter your feature name: ",
+		                " ", JOptionPane.PLAIN_MESSAGE,
+		                null,
+		                null,
+		                null);
+				//TO DO: Give user drop-down list of classes
+				String value = (String)JOptionPane.showInputDialog(
+		                featureFrame,
+		                "Enter your feature class: ",
+		                " ", JOptionPane.PLAIN_MESSAGE,
+		                null,
+		                null,
+		                null);
+				dataType.put(key, value);
+		        
+			};
+			return null;
+			
+		}
 
 }
