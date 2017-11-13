779c404f1aa9072e0ae366d33d19527429d52230
Darren Holden <darren.a.holden@outlook.com>
2017-11-13 12:28:34 -0500

    Complete Milestone2 classes

---
+++ CodersIncMilestone2/EuclideanKNN.java
@@ -0,0 +1,148 @@
+package CodersInc;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * EuclideanKNN extends the kNN abstract method and uses an euclidean distance metric for the calculations.
+ * The formula is [distance] = sqrt(sum((endPoint - origin) ^ 2)) where the sum adds the values for every dimension.
+ * 
+ * @author Darren
+ * @version Milestone 2
+ *
+ */
+public class EuclideanKNN extends kNN {
+	
+	/**
+	 * Constructor for EuclideanKNN. Takes a DimensionalSpace object that it will work in. Calls the kNN constructor.
+	 * 
+	 * @param ds	The DimensionalSpace the the funciton will work in.
+	 */
+	public EuclideanKNN(DimensionalSpace ds) {
+		super(ds);
+	}
+
+	/**
+	   * findKNN finds the target value for the given point using an euclidean KNN algorithm
+	   * with the k nearest points. Calls the super class's findValue funtion once the nearest neighbours
+	   * are found.
+	   * 
+	   * @param targetKey		The key to be found
+	   * @param targetPoint		The point whose value is to be found
+	   * @param neighbours		The number of nearest neighbours to query
+	   * 
+	   * @return				The unknown value of the given point
+	   */
+	public Cell findKNN(String targetKey, Point targetPoint, int neighbours) {
+	  	
+		NumericalDistance nDist = new NumericalDistance();
+		StringDistance sDist = new StringDistance();
+		
+		ds.findStatistics();
+		
+		ArrayList<Point> points = ds.getPoints();
+		
+		if (neighbours <= 0) neighbours = 1;
+	    if (neighbours > points.size()) neighbours = points.size();
+		
+	    ArrayList<Tuple<Float, Cell>> closestKNeighbours = new ArrayList<Tuple<Float, Cell>>(neighbours);
+		Tuple<Float, Cell> displacedPoint, placeHolder;
+		float distance;
+		boolean displacement;
+		HashMap<String, Cell> currentPtValues;
+		HashMap<String, Cell> targetPtValues = targetPoint.getStdValues();
+		
+		for (int i = 0; i < neighbours; i++) {
+			closestKNeighbours.add(i, null);
+		}
+		
+		for (Point pt: points) {
+			if (!(pt.equals(targetPoint))) {
+				displacement = false;
+				displacedPoint = null;
+				distance = 0;
+				currentPtValues = pt.getStdValues();
+				
+				for (String key: currentPtValues.keySet()) {
+					if (!(key.equals(targetKey))) {
+						if (currentPtValues.get(key) instanceof SimpleCell) {
+							if (((SimpleCell)currentPtValues.get(key)).getValue() instanceof String) {
+								distance += Math.pow(sDist.calcDistance((SimpleCell)targetPtValues.get(key), (SimpleCell)currentPtValues.get(key)), 2);
+							} else {
+								distance += Math.pow(nDist.calcDistance((SimpleCell)targetPtValues.get(key), (SimpleCell)currentPtValues.get(key)), 2);
+							}
+						} else {
+							distance += Math.pow(EuclideanComposite((CompositeCell) currentPtValues.get(key), (CompositeCell) targetPtValues.get(key)), 2);
+						}
+					}
+				}
+				
+				distance = (float) Math.sqrt(distance);
+				
+				for (int i = 0; i < neighbours; i ++) {
+					if (!displacement) {
+						// If the current spot has not been used yet
+						if (closestKNeighbours.get(i) == null) {
+							closestKNeighbours.set(i, new Tuple<Float, Cell>(distance, pt.getCell(targetKey)));
+							break;
+							
+						// If the current point is displacing other point(s)
+						} else if (distance < closestKNeighbours.get(i).getValue1()) {
+							displacedPoint = closestKNeighbours.get(i);
+							closestKNeighbours.set(i, new Tuple<Float, Cell>(distance, pt.getCell(targetKey)));
+							displacement = true;
+						}
+					} else {
+						// If the current spot has not been used yet
+						if (closestKNeighbours.get(i) == null) {
+							closestKNeighbours.set(i, new Tuple<Float, Cell>(distance, pt.getCell(targetKey)));
+							break;
+							
+						// If the current point is displacing other point(s)
+						} else if (distance < closestKNeighbours.get(i).getValue1()) {
+							placeHolder = closestKNeighbours.get(i);
+							closestKNeighbours.set(i, displacedPoint);
+							displacedPoint = placeHolder;
+						}
+					}
+				}
+			}
+		}
+		
+		Cell c = findValue(closestKNeighbours, targetKey);
+		targetPoint.addAttribute(c);
+		
+		return c;
+	}
+	
+	/**
+	 * Iterates through all of the sub cells in a CompositeCell and calculates the distance from that CompositeCell to
+	 * the target point's corresponding CompositeCell.
+	 * 
+	 * @param current	The CompositeCell of the non-target point
+	 * @param target	The CompositeCell of the target point
+	 * @return			The distance between the Cells
+	 */
+	public float EuclideanComposite(CompositeCell current, CompositeCell target) {
+	  	
+		NumericalDistance nDist = new NumericalDistance();
+		StringDistance sDist = new StringDistance();
+		ArrayList<Cell> subCells = current.getSubCells();
+		float distance = 0;
+		
+		for (Cell c: subCells) {
+			if (c instanceof SimpleCell) {
+				if (((SimpleCell) c).getValue() instanceof String) {
+					distance += Math.pow(sDist.calcDistance((SimpleCell) target.getSubCell(c.getKey()), (SimpleCell) c), 2);
+				} else {
+					distance += Math.pow(nDist.calcDistance((SimpleCell) target.getSubCell(c.getKey()), (SimpleCell) c), 2);
+				}
+			} else {
+				distance += EuclideanComposite((CompositeCell) c, (CompositeCell) target.getSubCell(c.getKey()));
+			}
+		}
+		
+		return (float) Math.sqrt(distance);
+	}
+
+}
---
+++ CodersIncMilestone2/MyTests.java
@@ -0,0 +1,229 @@
+package CodersInc;
+
+import java.util.ArrayList;
+import CodersInc.kNN.Tuple;
+import junit.framework.*;
+
+@SuppressWarnings("unchecked")
+public class MyTests extends TestCase {
+	
+	private DimensionalSpace ds;
+	
+	public void setUp() {
+		ds = new DimensionalSpace();
+	}
+	
+	public void testNumericalDistanceWithInteger() {
+		SimpleCell<Integer> c1 = new SimpleCell<Integer>("test1", 10);
+		SimpleCell<Integer> c2 = new SimpleCell<Integer>("test1", 7);
+		NumericalDistance nd = new NumericalDistance();
+		
+		float value = nd.calcDistance(c1, c2);
+		assertEquals(value, (float) 10 - 7);
+		
+		value = nd.calcDistance(c2, c1);
+		assertEquals(value, (float) 10 - 7);
+	}
+	
+	public void testNumericalDistanceWithFloat() {
+		SimpleCell<Float> c1 = new SimpleCell<Float>("test1", (float)3.76);
+		SimpleCell<Float> c2 = new SimpleCell<Float>("test1", (float)2.385);
+		NumericalDistance nd = new NumericalDistance();
+		
+		float value = nd.calcDistance(c1, c2);
+		assertTrue(Math.abs(value - ((float) 3.76 - 2.385)) < 0.000001 );
+		
+		value = nd.calcDistance(c2, c1);
+		assertTrue(Math.abs(value - ((float) 3.76 - 2.385)) < 0.000001 );
+	}
+	
+	public void testStringDistance() {
+		SimpleCell<String> c1 = new SimpleCell<String>("test1", "Cat");
+		SimpleCell<String> c2 = new SimpleCell<String>("test1", "Soup");
+		StringDistance sd = new StringDistance();
+		
+		float value = sd.calcDistance(c1, c2);
+		assertEquals(value, (float) 4);
+		
+		c2.setValue("Catch");
+		
+		value = sd.calcDistance(c1, c2);
+		assertEquals(value, (float) 2);
+
+		c2.setValue("Cat");
+		
+		value = sd.calcDistance(c1, c2);
+		assertEquals(value, (float) 0);
+	}
+	
+	public void testKNNCalculateSimpleCellInteger() {
+		Tuple<Float, Cell> t1 = new Tuple<Float, Cell>((float) 1, new SimpleCell<Integer>("1", 3));
+		Tuple<Float, Cell> t2 = new Tuple<Float, Cell>((float) 2, new SimpleCell<Integer>("1", 4));
+		Tuple<Float, Cell> t3 = new Tuple<Float, Cell>((float) 3, new SimpleCell<Integer>("1", 5));
+		
+		kNN testKNN = new EuclideanKNN(ds);
+		ArrayList<Tuple<Float, Cell>> list = new ArrayList<Tuple<Float, Cell>>(3);
+		list.add(0, t1);
+		list.add(1, t2);
+		list.add(2, t3);
+		
+		SimpleCell<Integer> c1 = (SimpleCell<Integer>) testKNN.calculateSimpleCell(list, "Integer", "1");
+		
+		assertEquals(c1.getKey(), "1");
+		
+		assertEquals((int)c1.getValue(), 4);
+	}
+	
+	public void testKNNCalculateSimpleCellFloat() {
+		Tuple<Float, Cell> t1 = new Tuple<Float, Cell>((float) 1, new SimpleCell<Float>("1", (float) 3.545));
+		Tuple<Float, Cell> t2 = new Tuple<Float, Cell>((float) 2, new SimpleCell<Float>("1", (float) 4.567));
+		Tuple<Float, Cell> t3 = new Tuple<Float, Cell>((float) 3, new SimpleCell<Float>("1", (float) 5.532));
+		
+		kNN testKNN = new EuclideanKNN(ds);
+		ArrayList<Tuple<Float, Cell>> list = new ArrayList<Tuple<Float, Cell>>(3);
+		list.add(0, t1);
+		list.add(1, t2);
+		list.add(2, t3);
+		
+		SimpleCell<Float> c1 = (SimpleCell<Float>) testKNN.calculateSimpleCell(list, "Float", "1");
+		
+		assertEquals(c1.getKey(), "1");
+		
+		assertEquals((float)c1.getValue(), (float) 4.548);
+	}
+	
+	public void testKNNCalculateSimpleCellString() {
+		Tuple<Float, Cell> t1 = new Tuple<Float, Cell>((float) 1, new SimpleCell<String>("1", "Cat"));
+		Tuple<Float, Cell> t2 = new Tuple<Float, Cell>((float) 2, new SimpleCell<String>("1", "Coder"));
+		Tuple<Float, Cell> t3 = new Tuple<Float, Cell>((float) 3, new SimpleCell<String>("1", "Dog"));
+		
+		kNN testKNN = new EuclideanKNN(ds);
+		ArrayList<Tuple<Float, Cell>> list = new ArrayList<Tuple<Float, Cell>>(3);
+		list.add(0, t1);
+		list.add(1, t2);
+		list.add(2, t3);
+		
+		SimpleCell<String> c1 = (SimpleCell<String>) testKNN.calculateSimpleCell(list, "String", "1");
+		
+		assertEquals(c1.getKey(), "1");
+		assertEquals(c1.getValue(), "Cat");
+		
+		t2.setValue2(new SimpleCell<String>("1", "Dog"));
+		
+		list.set(1, t2);
+		c1 = (SimpleCell<String>) testKNN.calculateSimpleCell(list, "String", "1");
+		
+		assertEquals(c1.getValue(), "Dog");
+	}
+	
+	public void testKNNCalculateCompositeCell() {
+		
+		CompositeCell cc1 = new CompositeCell("t1");
+		CompositeCell cc2 = new CompositeCell("t1");
+		
+		SimpleCell<Integer> c1I = new SimpleCell<Integer>("s1", 44);
+		SimpleCell<Float> c1F = new SimpleCell<Float>("s2", (float) 3.45);
+		SimpleCell<String> c1S = new SimpleCell<String>("s3", "Cat");
+		
+		SimpleCell<Integer> c2I = new SimpleCell<Integer>("s1", 54);
+		SimpleCell<Float> c2F = new SimpleCell<Float>("s2", (float) 4.52);
+		SimpleCell<String> c2S = new SimpleCell<String>("s3", "Dog");
+		
+		cc1.addCell(c1I);
+		cc2.addCell(c2I);
+		
+		cc1.addCell(c1F);
+		cc2.addCell(c2F);
+		
+		cc1.addCell(c1S);
+		cc2.addCell(c2S);
+		
+		ArrayList<Tuple<Float, Cell>> list = new ArrayList<Tuple<Float, Cell>>(2);
+		list.add(0, new Tuple<Float, Cell>((float) 1, cc1));
+		list.add(1, new Tuple<Float, Cell>((float) 2, cc2));
+		
+		kNN testKNN = new EuclideanKNN(ds);
+		
+		CompositeCell ccEnd = testKNN.calculateCompositeCell(list, cc1.getKey());
+		
+		assertEquals(ccEnd.getKey(), cc2.getKey());
+		assertEquals((String)((SimpleCell<String>)ccEnd.getSubCell("s3")).getValue(), "Cat");
+		assertTrue((float)(Math.abs((float)((SimpleCell<Float>)ccEnd.getSubCell("s2")).getValue()) - 3.985) < 0.000001);
+		assertEquals((int)((SimpleCell<Integer>)ccEnd.getSubCell("s1")).getValue(), 49);
+	}
+	
+	public void testEuclideanKNN() {
+		Point p1 = new Point();
+		Point p2 = new Point();
+		Point p3 = new Point();
+		
+		CompositeCell cc1 = new CompositeCell("t1");
+		CompositeCell cc2 = new CompositeCell("t1");
+		CompositeCell cc3 = new CompositeCell("t1");
+		
+		SimpleCell<Integer> c1I = new SimpleCell<Integer>("s1", 44);
+		SimpleCell<Float> c1F = new SimpleCell<Float>("s2", (float) 3.45);
+		SimpleCell<String> c1S = new SimpleCell<String>("s3", "Cat");
+		
+		SimpleCell<Integer> c2I = new SimpleCell<Integer>("s1", 54);
+		SimpleCell<Float> c2F = new SimpleCell<Float>("s2", (float) 4.52);
+		SimpleCell<String> c2S = new SimpleCell<String>("s3", "Dog");
+
+		SimpleCell<Integer> c3I = new SimpleCell<Integer>("s1", 66);
+		SimpleCell<Float> c3F = new SimpleCell<Float>("s2", (float) 5.26);
+		SimpleCell<String> c3S = new SimpleCell<String>("s3", "Dog");
+		
+		cc1.addCell(c1I);
+		cc2.addCell(c2I);
+		cc3.addCell(c3I);
+		
+		cc1.addCell(c1F);
+		cc2.addCell(c2F);
+		cc3.addCell(c3F);
+		
+		cc1.addCell(c1S);
+		cc2.addCell(c2S);
+		cc3.addCell(c3S);
+		
+		p1.addAttribute(cc1);
+		p2.addAttribute(cc2);
+		p3.addAttribute(cc3);
+
+		p1.addAttribute(new SimpleCell<Integer>("a1", 20));
+		p2.addAttribute(new SimpleCell<Integer>("a1", 28));
+		p3.addAttribute(new SimpleCell<Integer>("a1", 30));
+
+		p1.addAttribute(new SimpleCell<Float>("a2", (float) 158.964));
+		p2.addAttribute(new SimpleCell<Float>("a2", (float) 2.5169));
+		p3.addAttribute(new SimpleCell<Float>("a2", (float) 25.369));
+		
+		ds.addPt(p1);
+		ds.addPt(p2);
+		ds.addPt(p3);
+		
+		//ds.findStatistics();
+		
+		Point targetPoint = new Point();
+		
+		CompositeCell cc4 = new CompositeCell("t1");
+		SimpleCell<Integer> c4I = new SimpleCell<Integer>("s1", 37);
+		SimpleCell<Float> c4F = new SimpleCell<Float>("s2", (float) 5.92);
+		SimpleCell<String> c4S = new SimpleCell<String>("s3", "Fish");
+		cc4.addCell(c4I);
+		cc4.addCell(c4F);
+		cc4.addCell(c4S);
+		targetPoint.addAttribute(cc4);
+		targetPoint.addAttribute(new SimpleCell<Float>("a2", (float) 86.253));
+		
+		kNN testKNN = new EuclideanKNN(ds);
+		
+		Cell result = testKNN.findKNN("a1", targetPoint, 1);
+		assertEquals((int)((SimpleCell<Integer>)result).getValue(), 30);
+		
+		result = testKNN.findKNN("a1", targetPoint, 2);
+		assertEquals((int)((SimpleCell<Integer>)result).getValue(), 25);
+		
+		result = testKNN.findKNN("a1", targetPoint, 3);
+		assertEquals((int)((SimpleCell<Integer>)result).getValue(), 26);
+	}
+}
