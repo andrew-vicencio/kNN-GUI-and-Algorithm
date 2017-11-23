e14ec070da723c8bb9357a74c8e1409a39ba6c90
Gaby Hubert <gabyhubert2@gmail.com>
2017-11-22 21:32:00 -0500

    Added distance metric JComboBox to FeaturePanelSimple
    Also added methods to access it from the FeaturePanelSimpleController

--- View/FeaturePanelSimple.java
+++ View/FeaturePanelSimple.java
@@ -25,8 +25,8 @@ import Controllers.*;
 public class FeaturePanelSimple extends FeaturePanel{
 
 	//Things specialized for this particular input
-	private JLabel  value;
-	private JComboBox<String> featureClass;
+	private JLabel  featureTypeLabel, distanceMetricLabel;
+	private JComboBox<String> featureType, distanceMetric;
 
 	/**
 	 * Default constructor for a View.FeaturePanelSimple with no parent
@@ -39,14 +39,20 @@ public class FeaturePanelSimple extends FeaturePanel{
 	{
 		super(view, types, superFeatureName, tab);
 		String[] typesArray = types.toArray(new String[0]);
-		featureClass = new JComboBox<String>(typesArray);
-		value = new JLabel("Feature type: ");
+		String[] metricsArray = {"Example1", "Example2", "Example3"};
+		featureType = new JComboBox<String>(typesArray);
+		distanceMetric = new JComboBox<String>(metricsArray);
+		featureTypeLabel = new JLabel("Feature type: ");
+		distanceMetricLabel = new JLabel("Distance Metric:  ");
 
-		innerPanel.add(value);
-		innerPanel.add(featureClass);
+		innerPanel.add(featureTypeLabel);
+		innerPanel.add(featureType);
+		innerPanel.add(distanceMetricLabel);
+		innerPanel.add(distanceMetric);
         innerPanel.add(add);
 		controller = new FeaturePanelSimpleController(this);
 		add.addActionListener(controller);
+		
 	}
 	
 	/**
@@ -77,7 +83,7 @@ public class FeaturePanelSimple extends FeaturePanel{
 		String s="";
 		try
 		{
-			s = (String) featureClass.getSelectedItem();
+			s = (String) featureType.getSelectedItem();
 		}
 		catch(Exception e)
 		{
@@ -86,21 +92,20 @@ public class FeaturePanelSimple extends FeaturePanel{
 		return s;
 	}
 
-
-	
 	/**
 	 * Disables the JTextField and JComBobox
 	 */
 	public void disable()
     {
 		featureName.setEnabled(false);
-		featureClass.setEnabled(false);
+		featureType.setEnabled(false);
+		distanceMetric.setEnabled(false);
 		add.setEnabled(false);
 	}
-
-	public String getKeyParentName() {
-		// TODO Auto-generated method stub
-		return null;
+	
+	public String getDistanceMetric()
+	{
+		return (String) distanceMetric.getSelectedItem();
 	}
 }
 	
